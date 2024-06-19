7c8d76279d76656653fa93f7639e99db11f9ebff
Ritik2513 <ritikgupta2513@gmail.com>
2024-06-19 17:51:06 +0530

    jwt signup login logout

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import createTokenAndSaveCookie from "../jwt/generateToken.js";
 
+//signup
 export const signup = async (req, res) => {
   const { fullname, email, password, confirmPassword } = req.body;
 
@@ -8,6 +10,7 @@ export const signup = async (req, res) => {
     if (password !== confirmPassword) {
       return res.status(400).json({ error: "Password do not match" });
     }
+
     //find if user is already available
     const user = await User.findOne({ email });
     if (user) {
@@ -24,7 +27,47 @@ export const signup = async (req, res) => {
       password: hashPassword,
     });
     await newUser.save();
-    res.status(201).json({ message: "User Created successfully" });
+
+    if (newUser) {
+      createTokenAndSaveCookie(newUser._id, res);
+      res.status(201).json({ message: "User Created successfully", newUser });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
+
+//login
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email }); //databse user
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!user || !isMatch) {
+      return res.status(400).json({ error: "Invalid user Credential" });
+    }
+    //generate token while login as well
+    createTokenAndSaveCookie(user._id, res);
+    res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        _id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
+
+// logout
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt");
+    res.status(201).json({ message: "User logged out successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal Server error" });
