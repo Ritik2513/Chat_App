e821584170b7956418aeb4cf2d6c97d7857202e8
Ritik2513 <ritikgupta2513@gmail.com>
2024-06-20 15:10:29 +0530

    Signup & Login UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Left from "./home/Leftpart/Left";
 import Right from "./home/Rightpart/Right";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="flex h-screen overflow-y-auto no-scrollbar">
-      <Left />
-      <Right />
-    </div>
+    // <div className="flex h-screen overflow-y-auto no-scrollbar">
+    //   <Left />
+    //   <Right />
+    // </div>
+    <Signup/>
+    // <Login/>
   );
 }
 
