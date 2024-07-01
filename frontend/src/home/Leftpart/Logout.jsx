69457c23983baeca4d821945f62c308d9e5ba245
Ritik2513 <ritikgupta2513@gmail.com>
2024-07-01 16:55:39 +0530

    logout functionality applied

--- frontend/src/home/Leftpart/Logout.jsx
+++ frontend/src/home/Leftpart/Logout.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiLogOutCircle } from "react-icons/bi";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 function Logout() {
+  const [loading, setLoading] = useState(false);
+  const handleLogout = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post("/api/user/logout");
+      localStorage.removeItem("ChatApp");
+      Cookies.remove("jwt");
+      setLoading(false);
+      alert("Logged out successfully");
+      window.location.reload();
+    } catch (error) {
+      console.log("Error in logout" + error);
+    }
+  };
   return (
     <div className="h-[10vh]">
       <div>
-        <BiLogOutCircle className="text-5xl text-white hover:bg-slate-700 duration-300 cursor-pointer rounded-full p-2 ml-2 mt-1" />
+        <BiLogOutCircle
+          onClick={handleLogout}
+          className="text-5xl text-white hover:bg-slate-700 duration-300 cursor-pointer rounded-full p-2 ml-2 mt-1"
+        />
       </div>
     </div>
   );
